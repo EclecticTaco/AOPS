7230ebb27906d129f48e978da1d0326a6572cf07
Calvin <calvin@pop-os.localdomain>
2021-03-18 22:45:01 -0700

    added test cases that only go in one direction

--- test/testCases.js
+++ test/testCases.js
@@ -34,13 +34,32 @@ const pyramid6 = [
     [9,3,1,2],
     [60,7,4,5,6]
 ] // 192
+
+const pyramid7 = [
+    [1],
+    [2,3],
+    [6,8,10],
+    [9,3,1,2],
+    [60,7,4,5,6]
+] // 6480 (left only)
+
+const pyramid8 = [
+    [2],
+    [4,3],
+    [3,2,6],
+    [2,9,5,2],
+    [10,5,2,15,5]
+] // 360 (right only) 
+
 const cases = [
     [pyramid1, 2, ['L', 'R']],
     [pyramid2, 720, ['L', 'R', 'L', 'L']],
     [pyramid3, 2, ['L']],
     [pyramid4, 1, []],
     [pyramid5, 4, []],
-    [pyramid6, 192, ['L', 'R', 'L', 'R']]
+    [pyramid6, 192, ['L', 'R', 'L', 'R']],
+    [pyramid7, 6480, ['L', 'L', 'L', 'L']],
+    [pyramid8, 360, ['R', 'R', 'R', 'R']]
 ]
 
 for (let i = 0; i < cases.length; i += 1) {
