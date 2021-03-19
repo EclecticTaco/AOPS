508cf45fc4ab9644420c4afc953179bf42f95b9a
Calvin <calvin@pop-os.localdomain>
2021-03-18 22:28:43 -0700

    cleaned up old tests

--- test/testCases.js
+++ test/testCases.js
@@ -27,16 +27,6 @@ const pyramid5 = [
     [4],
 ] // 4
 
-
-
-const test = (output, expected) => {
-    if (output.length !== expected.length) return false
-    for (let i = 0; i < output.length; i += 1) {
-        if (output[i] !== expected[i]) return false
-    }
-    return true
-}
-
 const cases = [
     [pyramid1, 2, ['L', 'R']],
     [pyramid2, 720, ['L', 'R', 'L', 'L']],
@@ -57,26 +47,3 @@ for (let i = 0; i < cases.length; i += 1) {
     })
 } 
 
-/* let pass = true
-let fail = []
-for (let i = 0; i < cases.length; i += 1) {
-    const aPyramid = cases[i][0];
-    const target = cases[i][1]
-    const expected = cases[i][2];
-    const output = pyramid.pyramidDescent(aPyramid, target)
-    const result = test(output, expected)
-    if (!result) {
-        pass = false
-        fail.push(`output of ${aPyramid}: `, output, `expected result: ${expected} `)
-    }
-} */
-
-/* if (pass) {
-    console.log('All test cases pass!')
-} else {
-    console.log('One more more test cases failed. Failed test cases: ')
-    console.log(JSON.stringify(fail))
-}
-
-
- */
