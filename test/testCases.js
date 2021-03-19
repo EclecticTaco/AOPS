996f3ed7aafc75d28e8ee7a792af2c8cfb1c8fcd
Calvin <calvin@pop-os.localdomain>
2021-03-18 22:38:02 -0700

    added deeper pyramid

--- test/testCases.js
+++ test/testCases.js
@@ -27,12 +27,20 @@ const pyramid5 = [
     [4],
 ] // 4
 
+const pyramid6 = [
+    [1],
+    [2,3],
+    [6,8,10],
+    [9,3,1,2],
+    [60,7,4,5,6]
+] // 192
 const cases = [
     [pyramid1, 2, ['L', 'R']],
     [pyramid2, 720, ['L', 'R', 'L', 'L']],
     [pyramid3, 2, ['L']],
     [pyramid4, 1, []],
     [pyramid5, 4, []],
+    [pyramid6, 192, ['L', 'R', 'L', 'R']]
 ]
 
 for (let i = 0; i < cases.length; i += 1) {
